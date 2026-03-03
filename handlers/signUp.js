ef592767378053083530aaa6d3424e4493fcfb03
IRENE JOY TRINCHERA <irenejoy15@gmail.com>
2026-03-03 15:06:41 +0800

    first commit

---
+++ handlers/signUp.js
@@ -0,0 +1,44 @@
+//Import the requrired AWS Cognito SDK CLASSES
+//Cognito Identity Profiler Client used to communicate to cognito
+//SignUpCommand : Used to send sign-up request
+
+const {CognitoIdentityProviderClient,SignUpCommand} = require('@aws-sdk/client-cognito-identity-provider');
+
+const client = new CognitoIdentityProviderClient({ reqion:'ap-southeast-2' });
+
+//specify the cognito app client id
+//the app client id tells cognito which app making the request
+
+const CLIENT_ID = 'bbbvsdudg6e9n0h6ujv4sqlbi';
+
+//DEFINE A LAMBDA FUNCTION TO SEND SIGNUP REQUEST
+
+exports.signUp = async (event) => {
+    const {email,fullName,password} = JSON.parse(event.body);
+
+    //Prepare parameters required by Cognito signUpcommand
+    const params = {
+        ClientId: CLIENT_ID,
+        Username: email,
+        Password: password,
+        UserAttributes:[
+            {Name:'email',Value:email},
+            {Name:'name',Value:fullName},
+        ]
+    };
+
+    try{
+        //CREATE SIGN UP COMMAND OBJECT WITH THE PREPARED PARAMETERS
+        const command = new SignUpCommand(params);
+        await client.send(command);
+        return {
+            statusCode: 200,
+            body: JSON.stringify({msg:'USER SUCCESSFULLY SIGN UP'}),
+        };
+    }catch(error){
+        return {
+            statusCode:500,
+            body: JSON.stringify({error:'UNEXPECTED ERROR',error:error.message}),
+        };
+    }
+};
