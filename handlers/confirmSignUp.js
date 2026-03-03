44f3c17a9cc1e3e60ed0c6489fbaabbfe67523c8
IRENE JOY TRINCHERA <irenejoy15@gmail.com>
2026-03-03 15:50:19 +0800

    Verify Handler

---
+++ handlers/confirmSignUp.js
@@ -0,0 +1,34 @@
+const {CognitoIdentityProviderClient,ConfirmSignUpCommand} = require('@aws-sdk/client-cognito-identity-provider');
+
+const client = new CognitoIdentityProviderClient({ region:'ap-southeast-2' });
+
+//specify the cognito app client id
+//the app client id tells cognito which app making the request
+
+const CLIENT_ID = 'bbbvsdudg6e9n0h6ujv4sqlbi';
+
+exports.confirmSignUp = async (event) => {
+    const {email,confirmationCode} = JSON.parse(event.body);
+
+    //Prepare parameters required by Cognito confirmSignUpCommand
+    const params = {
+        ClientId: CLIENT_ID,
+        Username: email,
+        ConfirmationCode: confirmationCode
+    };
+
+    try{
+        //CREATE CONFIRM SIGN UP COMMAND OBJECT WITH THE PREPARED PARAMETERS
+        const command = new ConfirmSignUpCommand(params);
+        await client.send(command);
+        return {
+            statusCode: 200,
+            body: JSON.stringify({msg:'USER SUCCESSFULLY CONFIRMED'}),
+        };
+    }catch(error){
+        return {
+            statusCode:500,
+            body: JSON.stringify({error:'UNEXPECTED ERROR',error:error.message}),
+        };
+    }
+};
